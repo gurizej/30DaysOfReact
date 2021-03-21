24edca83cc83184a166160fae48f07ca237ab2a3
Guri <guri.zej@gmail.com>
2021-03-20 22:15:52 -0400

    Day 17 done

---
+++ day17/src/index.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink,
+  Switch,
+  Redirect,
+  Prompt,
+  withRouter,
+} from 'react-router-dom'
+
+// Home component
+const Home = (props) => <h1>Welcome Home</h1>
+// About component
+const About = (props) => <h1>About Us</h1>
+// Contact component
+const Contact = (props) => <h1>Contact us</h1>
+// Challenge component
+const Challenges = (props) => (
+  <div>
+    <h1>30 Days Of React Challenge</h1>
+  </div>
+)
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div className='App'>
+          <ul>
+            <li>
+              <NavLink to='/'>Home</NavLink>
+            </li>
+            <li>
+              <NavLink to='/about'>About</NavLink>
+            </li>
+            <li>
+              <NavLink to='/contact'>Contact</NavLink>
+            </li>
+            <li>
+              <NavLink to='/challenges'>Challenges</NavLink>
+            </li>
+          </ul>
+
+          <Switch>
+            <Route path='/about' component={About} />
+            <Route path='/contact' component={Contact} />
+            <Route path='/challenges' component={Challenges} />
+            <Route path='/' component={Home} />
+          </Switch>
+        </div>
+      </Router>
+    )
+  }
+}
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
